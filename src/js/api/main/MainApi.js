904d97f99baa2b1967313f8b60d07aca86ef16ec
Kristina Gromova <christina28romova@yandex.ru>
2020-06-28 09:50:55 +0300

    header

--- src/js/api/main/MainApi.js
+++ src/js/api/main/MainApi.js
@@ -1,27 +1,22 @@
-/* eslint-disable class-methods-use-this */
-// eslint-disable-next-line import/prefer-default-export
 export default class MainApi {
   constructor(baseUrl) {
     this.baseUrl = baseUrl;
   }
 
-  makeFetch(url, method = 'GET', body = undefined) {
+  /* makeFetch(url, method = 'GET', body = undefined) {
     if (body) {
-      // eslint-disable-next-line no-param-reassign
       body = JSON.stringify(body);
     }
-    // eslint-disable-next-line no-undef
+
     return fetch(`${this.baseUrl}/${url}`, {
       method,
       credentials: 'include',
       headers: {
         'Content-Type': 'application/json',
-        // eslint-disable-next-line no-undef
         authorization: `Bearer ${localStorage.getItem('token')}`,
       },
       body,
     })
-      // eslint-disable-next-line consistent-return
       .then((res) => {
         if (res.ok) {
           return res.json();
@@ -30,15 +25,27 @@ export default class MainApi {
       .catch((err) => {
         throw err;
       });
-  }
+  } */
 
   getUser() {
-    return this.makeFetch('users/me');
+    // eslint-disable-next-line no-undef
+    return fetch(`${this.baseUrl}/users/me`, {
+      headers: {
+        // eslint-disable-next-line no-undef
+        authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.regect(res.status);
+      });
   }
+}
 
-  signup(email, password, name) {
-    // eslint-disable-next-line object-shorthand
-    return this.makeFetch('signup', 'POST', { email: email, password: password, name: name });
+/* signup(email, password, name) {
+    return this.makeFetch('signup', 'POST', { email, password, name });
   }
 
   signin(email, password) {
@@ -64,4 +71,4 @@ export default class MainApi {
   deleteArticle(articleId) {
     return this.makeFetch(`{articles/${articleId}`, 'DELETE');
   }
-}
+} */
